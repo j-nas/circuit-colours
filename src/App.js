e686b02f271f188312286b017784061207894924
Jon Naske <jonnaske@hotmail.com>
2021-06-29 07:47:41 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [circuit, setCircuit] = useState(1)
+  const [colour, setColour] = useState("red")
+  
+  const handleChange = (e) =>{
+    const value = e.target.value;
+    setCircuit(value)
+  }
+  
+  const getColour = () => {
+    let colourOutput = ""
+    switch (circuit % 6) {
+      case 0:
+        colourOutput = "blue"
+        break
+      case 1:
+        colourOutput = "red"
+        break
+      case 2:
+        colourOutput = "red"  
+        break
+      case 3:
+        colourOutput = "black"
+        break
+      case 4: 
+        colourOutput = "black"
+        break
+      case 5: 
+        colourOutput = "blue"
+        break
+      default:
+        break
+    }
+    setColour(colourOutput)
+  }
+  
+  
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+       Circuit {circuit} is {colour}
+     </div>
+     
+       <input 
+        type="number" 
+        name="number" 
+        id="number"
+        value={circuit} 
+        onChange={handleChange}
+       />
+       <button onClick={getColour}>
+         Check Colour
+       </button>
+
     </div>
   );
 }
