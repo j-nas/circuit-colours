64eaad890bae99edb94d72b89f27e7d6add4e151
Jon Naske <jonnaske@hotmail.com>
2022-01-13 22:58:54 -0800

    moved helpers to own modules

---
+++ src/utils/keypadLayout.js
@@ -0,0 +1,13 @@
+export const keys = [
+  { label: "1", id: "1", type: "number" },
+  { label: "2", id: "2", type: "number" },
+  { label: "3", id: "3", type: "number" },
+  { label: "4", id: "4", type: "number" },
+  { label: "5", id: "5", type: "number" },
+  { label: "6", id: "6", type: "number" },
+  { label: "7", id: "7", type: "number" },
+  { label: "8", id: "8", type: "number" },
+  { label: "9", id: "9", type: "number" },
+  { label: "0", id: "0", type: "zero" },
+  { label: "CLR", id: "CLR", type: "clear" },
+]
